50f7c504458d6ee056e05a39261110033d68e46d
matias-garcia-17 <123090482+matias-garcia-17@users.noreply.github.com>
2023-01-19 17:12:41 -0300

    Add files via upload

---
+++ Proyecto.js
@@ -0,0 +1,16 @@
+
+document.getElementById('boton_contacto').addEventListener('click', function(){
+    document.getElementById('texto_contacto').style.display = "block";
+})
+
+document.getElementById('boton_aptitudes').addEventListener('click', function(){
+    document.getElementById('texto_aptitudes').style.display = "block";
+})
+
+document.getElementById('boton_idiomas').addEventListener('click', function(){
+    document.getElementById('texto_idiomas').style.display = "block";
+})
+
+document.getElementById('boton_afiliaciones').addEventListener('click', function(){
+    document.getElementById('texto_afiliaciones').style.display = "block";
+})
